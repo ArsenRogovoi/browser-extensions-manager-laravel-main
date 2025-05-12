664dc9055f36b377ad2a3ce65c01f307f1845da4
Arsen <arrog13@gmail.com>
2025-05-13 00:16:25 +0300

    project initialization

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/favicon-32x32.png">
+
+    <title>Frontend Mentor | Browser extensions manager UI</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+
+</html>
