a9990a5d5d7194a4f39fc655f31c18ed7fee4a4f
Arsen <arrog13@gmail.com>
2025-05-13 07:04:43 +0300

    initial data added in extensions.json. extension table added. extension seeder added.

---
+++ database/seeders/ExtensionSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use File;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ExtensionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // File::get - loads file text as a string
+        $json = File::get(database_path('data/extensions.json'));
+        $extensions = json_decode($json, true);
+        $extensions = array_map(function ($item) {
+            return collect($item)
+                ->except('isActive')
+                ->merge([
+                    'is_active' => $item['isActive'] ?? false,
+                ])
+                ->all();
+        }, $extensions);
+
+        DB::table('extensions')->insert($extensions);
+    }
+}
