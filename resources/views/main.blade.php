6d7c08e2b5919fc29b707dfbb5ad3caa91bccc45
Arsen <arrog13@gmail.com>
2025-05-23 11:58:45 +0300

    extensions grid has been built

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,11 +1,12 @@
 <x-layout>
     <section class="container">
-        <h1 class="text-2xl font-extrabold mt-8 mb-4 text-center dark:text-neutral-0">Extensions List</h1>
+        <h1 class="text-2xl text-neutral-900 font-extrabold mt-8 mb-4 text-center dark:text-neutral-0">Extensions List
+        </h1>
+        {{-- filter menu start: --}}
         <ul class="flex justify-center gap-2 mb-6">
             <li
                 class="border border-red-700 rounded-4xl px-4 py-1 bg-red-500 text-white dark:text-neutral-800 dark:border-neutral-800">
                 All</li>
-            {{-- <li class="px-4 py-1 rounded-4xl text-white bg-red-500 shadow-2xs shadow-red-700 inset-shadow-2xs inset-shadow-red-700">All</li> --}}
             <li
                 class="border border-neutral-300 rounded-4xl px-4 py-1 bg-neutral-0 dark:bg-neutral-700 dark:border-neutral-600">
                 Active</li>
@@ -13,59 +14,31 @@
                 class="border border-neutral-300 rounded-4xl px-4 py-1 bg-neutral-0 dark:bg-neutral-700 dark:border-neutral-600">
                 Inactive</li>
         </ul>
-
-        <article class="p-3 border border-neutral-300 rounded-2xl bg-neutral-0 dark:border-neutral-600">
-            <div class="flex items-start gap-3">
-                <img src="/storage/images/logo-devlens.svg" alt="devlens logo">
-                <div>
-                    <h2 class="font-bold">DevLens</h2>
-                    <p>Quickly inspect page layouts and visualize element boundaries.</p>
-                </div>
-            </div>
-        </article>
-
-        StyleSpy
-        Instantly analyze and copy CSS from any webpage element.
-        Remove
-
-        SpeedBoost
-        Optimizes browser resource usage to accelerate page loading.
-        Remove
-
-        JSONWizard
-        Formats, validates, and prettifies JSON responses in-browser.
-        Remove
-
-        TabMaster Pro
-        Organizes browser tabs into groups and sessions.
-        Remove
-
-        ViewportBuddy
-        Simulates various screen resolutions directly within the browser.
-        Remove
-
-        Markup Notes
-        Enables annotation and notes directly onto webpages for collaborative debugging.
-        Remove
-
-        GridGuides
-        Overlay customizable grids and alignment guides on any webpage.
-        Remove
-
-        Palette Picker
-        Instantly extracts color palettes from any webpage.
-        Remove
-
-        LinkChecker
-        Scans and highlights broken links on any page.
-        Remove
-
-        DOM Snapshot
-        Capture and export DOM structures quickly.
-        Remove
-
-        ConsolePlus
-        Enhanced developer console with advanced filtering and logging.
-        Remove
+        {{-- filter menu end --}}
+        {{-- extensions grid start: --}}
+        <div class="flex flex-col justify-center gap-2.5 mb-6">
+            @foreach ($extensions as $extension)
+                <article
+                    class="p-3 border border-neutral-300 rounded-2xl bg-neutral-0 dark:bg-neutral-700 dark:border-neutral-600">
+                    <div class="flex items-start gap-3 mb-4">
+                        <img src="{{ $extension->logo }}" alt="devlens logo">
+                        <div>
+                            <h2 class="mb-1 text-neutral-900 text-lg font-bold dark:text-neutral-0">
+                                {{ $extension->name }}</h2>
+                            <p class="text-neutral-600 text-sm/tight dark:text-neutral-300 sm:text-base/tight">{{ $extension->description }}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex justify-between">
+                        <button
+                            class="border border-neutral-300 rounded-4xl text-sm text-neutral-900 font-semibold px-3 py-1 bg-neutral-0 
+                            dark:bg-neutral-700 dark:border-neutral-600 dark:text-neutral-0"
+                            type="button">Remove</button>
+                        <input type="checkbox" name="" id="">
+                    </div>
+                </article>
+            @endforeach
+        </div>
+        {{-- extensions grid end --}}
     </section>
 </x-layout>
